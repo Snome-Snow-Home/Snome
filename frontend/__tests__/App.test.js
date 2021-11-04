3c5f214fd3795f005a3d900390c0a439160faddc
Raphael <raphspies@gmail.com>
2021-11-04 11:36:30 -0700

    configured prettier

--- frontend/__tests__/App.test.js
+++ frontend/__tests__/App.test.js
@@ -1,8 +1,8 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import App from "../App";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../App';
 
-test("renders correctly", () => {
+test('renders correctly', () => {
   const tree = renderer.create(<App />).toJSON();
   expect(tree).toMatchSnapshot();
 });
