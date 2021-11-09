6fa61913bde894677cec2cf9b8d7417110f26e1a
Zach <zachary.bretz@gmail.com>
2021-11-08 20:40:08 -0700

    Committing prior to merge

--- backend/server/controllers/get.js
+++ backend/server/controllers/get.js
@@ -7,9 +7,9 @@ module.exports = {
   getAll: async (req, res) => {
     try {
       const model = req.path.slice(1);
-      res.status(200).send(model);  // DUMMY RESPONSE
-      // let data = await get.getAll(model);
-      // res.status(200).send(data);
+      // res.status(200).send(model);  // DUMMY RESPONSE
+      let data = await get.getAll(model);
+      res.status(200).send(data);
     } catch(err) {
       console.log(`SERVER ERROR: ${err}`)
       res.status(400).send(err);
--- backend/server/models/get.js
+++ backend/server/models/get.js
@@ -7,6 +7,13 @@ module.exports = {
     try {
       // http://vitaly-t.github.io/pg-promise/Database.html#manyOrNone
       let result = await db.manyOrNone(`SELECT * FROM $1`, [model])
+
+      // let result = await db.manyOrNone(`SELECT * FROM snome`, [model])
+
+    //   let result = await db.manyOrNone(`  SELECT table_name
+    //   FROM information_schema.tables
+    //  WHERE table_schema='public'
+    //    AND table_type='BASE TABLE';`)
       return result;
     } catch(err) {
       console.log(`DATABASE ERROR: ${err}`)
