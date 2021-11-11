dd5efb9b9690cefa2ba5a12fbf2657d227b47265
Sam Schnaible II <sschnaible83@gmail.com>
2021-11-10 16:23:39 -0800

    write get request for 1 snome

--- backend/server/routes.js
+++ backend/server/routes.js
@@ -6,6 +6,8 @@ const router = require('express').Router();
 /* FOR BAREBONES TESTING ONLY */
 router.get('/snome', controller.get.getAll);
 router.get('/match', controller.get.getAll);
+
+router.get('/snome/:id', controller.get.getOne);
 // router.post('/snomes', controller.post.create);
 
 module.exports = router;
