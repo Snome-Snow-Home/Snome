9253f36bb6e8c5d931e95298b294114505b2a9a5
Sam Schnaible II <sschnaible83@gmail.com>
2021-11-11 15:41:36 -0800

    write handler function for snome POST request

--- backend/server/controllers/post.js
+++ backend/server/controllers/post.js
@@ -3,7 +3,14 @@ const { post } = require('../models');
 /* define post request handlers here */
 
 module.exports = {
-
+  create: async (req, res) => {
+    try {
+      let data = await create.post(req.body);
+      res.status(201).send(data);
+    } catch(err) {
+      res.status(400).send(err);
+    }
+  }
   // <TEMPLATE>: async (req, res) => {
   //   try {
   //     let data = await get.<TEMPLATE>(req.body);
