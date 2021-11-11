d658f04536876515279fc2370a643d894191a59a
Sam Schnaible II <sschnaible83@gmail.com>
2021-11-10 18:22:52 -0800

    write delete request for snomes

--- backend/server/routes.js
+++ backend/server/routes.js
@@ -19,4 +19,8 @@ router.patch('/snome/:id', controller.patch.updateSnome);
 
 // router.post('/snomes', controller.post.create);
 
+/* DELETE REQUESTS */
+
+router.delete('/snome/:id', controller.deletes.deleteSnome);
+
 module.exports = router;
