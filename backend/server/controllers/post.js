a15ced8de92331fc89b41228eb90140ea6a90d40
Sam Schnaible II <sschnaible83@gmail.com>
2021-11-11 17:26:08 -0800

    write POST request handler for creating new user

--- backend/server/controllers/post.js
+++ backend/server/controllers/post.js
@@ -11,7 +11,17 @@ module.exports = {
       console.log(`SERVER SIDE ERROR - POST: ${err}`)
       res.status(400).send(err);
     }
-  }
+  },
+
+  createUser: async (req, res) => {
+    try {
+      await post.createUser(req.body);
+      res.status(201).send('Success!');
+    } catch(err) {
+      console.log(`SERVER SIDE ERROR - POST: ${err}`);
+      res.status(400).send(err);
+    }
+  },
   // <TEMPLATE>: async (req, res) => {
   //   try {
   //     let data = await get.<TEMPLATE>(req.body);
