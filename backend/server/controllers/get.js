2802aba7e33e052e336d0980e73ceb96edb51258
Sam Schnaible II <sschnaible83@gmail.com>
2021-11-11 17:47:34 -0800

    write handler function for snome_user GET request

--- backend/server/controllers/get.js
+++ backend/server/controllers/get.js
@@ -29,6 +29,16 @@ module.exports = {
     }
   },
 
+  getUser: async (req, res) => {
+    try {
+      let data = await get.getUser(req.params.id);
+      res.status(200).send(data);
+    } catch(err) {
+      console.log(`SERVER ERROR: ${err}`);
+      res.status(400).send(err);
+    }
+  },
+
   // <TEMPLATE>: async (req, res) => {
   //   try {
   //     let data = await get.<TEMPLATE>(req.body);
