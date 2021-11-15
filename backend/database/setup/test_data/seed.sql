665c197c8cc7c964edce0b8a9d6a64a33443e8cd
thaysom22 <thaysom22@gmail.com>
2021-11-15 23:14:57 +0000

    restructure test data loading for docker container postegres instance

---
+++ backend/database/setup/test_data/seed.sql
@@ -0,0 +1,11 @@
+-- insert test data from .csv files into testdb
+
+-- \copy <target_table>
+-- from <file.csv>
+
+\copy snome from '/setup/test_data/snome.csv' delimiter ',' csv header;
+
+\copy snome_user from '/setup/test_data/user.csv' delimiter ',' csv header;
+
+\copy snome_like from '/setup/test_data/like.csv' delimiter ',' csv header;
+
