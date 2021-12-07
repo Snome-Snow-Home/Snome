9b1ec7abeee5af5d9a137e4628dbf19431efda10
Mohamed Metwalli <m.metwalli@outlook.com>
2021-12-07 13:33:13 -0500

    Update create snome in conterllers.get

--- backend/server/routes/routes.js
+++ backend/server/routes/routes.js
@@ -8,7 +8,7 @@ module.exports = router;
 /* SNOME */
 router.post(
   "/snome",
-  uploadSnomePhotos.array("snome_photos"),
+  uploadSnomePhotos.any("snome_photos"),
   controller.post.createSnome
 );
 router.get("/snome", controller.get.getAll);
@@ -27,7 +27,7 @@ router.delete("/user/:id", controller.remove.deleteUser);
 // TODO create, update, delete location? (admin only)
 router.get("/location", controller.get.getAll);
 router.get("/location/:id", controller.get.getOne);
-router.get("/snome/:location_id", controller.get.getSnomeByLocationId)
+router.get("/snome/location/:id", controller.get.getSnomeByLocationId);
 
 /* LIKES */
 router.post("/like", controller.post.createLike);
