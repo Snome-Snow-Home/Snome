73c532d864a6a6e1dd8ef10a2704d667a9f3736c
Amy Moseman <amoseman18@gmail.com>
2022-01-10 16:42:29 -0700

    had to refractor the form so it would send the correct data onsubmit to backend

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -82,38 +82,72 @@ const styles = StyleSheet.create({
 
 export default function CreateUser(props) {
     const [error, setError] = useState(null);
+    // const [formObject, setFormObject] = useState({
+    //     //need to change name to nameText on SQL model
+    //     nameText: '',
+    //     email: '',
+    //     address: '',
+    //     city: '',
+    //     state: '',
+    //     zipCode: '',
+    //     password: '',
+    //     confirmPassword: '',
+    // });
 
-    const form = useForm({
-        initialValues: {
-            name: '',
-            email: '',
-            address: '',
-            city: '',
-            state: '',
-            zipCode: '',
-            password: '',
-            confirmPassword: '',
-        },
-        validationRules: {
-            name: (value) => /^[a-z ,.'-]+$/i.test(value),
-            email: (value) => /^\S+@\S+$/.test(value),
-            city: (value) =>
-                /^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/.test(
-                    value
-                ),
-            state: (value) => value.trim().length >= 2,
-            zipCode: (value) => /^[0-9]{5}(?:-[0-9]{4})?$/.test(value),
-            password: (value) => /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$/.test(value),
+    const [nameText, setNameText] = useState("")
+    const [email, setEmail] = useState("")
+    const [address, setAddress] = useState("")
+    const [city, setCity] = useState("")
+    const [state, setState] = useState("")
+    const [zipCode, setZipcode] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConformPassword] = useState("")
 
-            confirmPassword: (val, values) => val === values.password,
-        },
-    });
 
-    function handleSubmit(values) {
-        form.validate()
-        console.log("values: ", values)
+    // const form = useForm({
+    //     initialValues: {
+    //         name: '',
+    //         email: '',
+    //         address: '',
+    //         city: '',
+    //         state: '',
+    //         zipCode: '',
+    //         password: '',
+    //         confirmPassword: '',
+    //     },
+    // validationRules: {
+    //     name: (value) => /^[a-z ,.'-]+$/i.test(value),
+    //     email: (value) => /^\S+@\S+$/.test(value),
+    //     city: (value) =>
+    //         /^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/.test(
+    //             value
+    //         ),
+    //     state: (value) => value.trim().length >= 2,
+    //     zipCode: (value) => /^[0-9]{5}(?:-[0-9]{4})?$/.test(value),
+    //     password: (value) => /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$/.test(value),
+
+    //     confirmPassword: (val, values) => val === values.password,
+    // },
+    // });
+
+    function handleSubmit(e) {
+        e.preventDefault
+        // form.validate()
+        //now we can 
+        console.log('user created')
+        console.log({
+            "nameText": nameText,
+            "email": email,
+            "address": address,
+            "city": city,
+            "zipcode": zipCode,
+            "password": password,
+            "confirmed password": confirmPassword
+        })
+
+        // console.log("values: ", values)
         //should this be form.error?
-        console.log("errors: ", form.errors)
+        // console.log("errors: ", form.errors)
     }
 
     return (
@@ -127,7 +161,7 @@ export default function CreateUser(props) {
             />
             <Text style={{ fontSize: 20, textAlign: "center" }}>New User? Sign up here</Text>
             <View
-                onSubmit={form.onSubmit((values) => handleSubmit(values))}
+            // onSubmit={form.onSubmit((values) => handleSubmit(values))}
             >
 
                 <Text style={styles.horizontal}>
@@ -139,16 +173,22 @@ export default function CreateUser(props) {
                     placeholder="Name"
                     type="text"
                     required
-                    value={form.values.name}
-                    autoCorrect="false"
-                    onChange={(event) => {
-                        form.setFieldValue('name', event.target.value);
-                        form.validate("name")
-                    }}
-                    //should this be unpluralized again>? errors->error
-                    style={form.errors.email ? styles.invalidInput : styles.formInput}
+                    value={nameText}
+                    // onChangeText={(newValue) => setFormObject.nameText(newValue)}
+                    // onChange={handleInputChange}
+                    onChangeText={setNameText}
+                // onChange={(event) => {
+                //     setFormObject('nameText', event.target.value);
+                //     console.log(setFormObject)
+                // }}
+                // onChange={(event) => {
+                //     form.setFieldValue('name', event.target.value);
+                //     form.validate("name")
+                // }}
+                //should this be unpluralized again>? errors->error
+                // style={form.errors.email ? styles.invalidInput : styles.formInput}
                 />
-                <ErrorMessage errorName={form.errors.name} errorId={"name-errorBox"} errorMessage={"includes invalid characters"} />
+                {/* <ErrorMessage errorName={form.errors.name} errorId={"name-errorBox"} errorMessage={"includes invalid characters"} /> */}
 
                 <Text style={styles.horizontal}>
                     <Text style={styles.label} htmlFor="email">Email: </Text>
@@ -159,11 +199,15 @@ export default function CreateUser(props) {
                     placeholder="Email"
                     type="text"
                     required
-                    value={form.values.email}
-                    onChange={(event) => form.setFieldValue('email', event.target.value)}
-                    style={form.errors.email ? styles.invalidInput : styles.formInput}
+                    // name='email'
+                    value={email}
+                    onChangeText={setEmail}
+                // onChangeText={handleText}
+                // onChange={(event) => form.setFieldValue('email', event.target.value)}
+                // style={form.errors.email ? styles.invalidInput : styles.formInput}
                 />
-                <ErrorMessage errorName={form.errors.email} errorId={"email-errorBox"} errorMessage={"invalid email address"} />
+
+                {/* <ErrorMessage errorName={form.errors.email} errorId={"email-errorBox"} errorMessage={"invalid email address"} /> */}
 
                 <Text style={styles.horizontal}>
                     <Text style={styles.label} htmlFor="address">Address: </Text>
@@ -174,11 +218,14 @@ export default function CreateUser(props) {
                     placeholder="Address"
                     type="text"
                     required
-                    value={form.values.address}
-                    onChange={(event) => form.setFieldValue('address', event.target.value)}
-                    style={styles.formInput}
+                    //name='address'
+                    value={address}
+                    onChangeText={setAddress}
+                // onChange={(event) => form.setFieldValue('address', event.target.value)}
+                // style={styles.formInput}
                 />
-                <ErrorMessage errorName={form.errors.address} errorId={"address-errorBox"} errorMessage={"invalid address"} />
+
+                {/* <ErrorMessage errorName={form.errors.address} errorId={"address-errorBox"} errorMessage={"invalid address"} /> */}
 
                 <Text style={styles.horizontal}>
                     <Text style={styles.label} htmlFor="city">City: </Text>
@@ -189,11 +236,16 @@ export default function CreateUser(props) {
                     placeholder="City"
                     type="text"
                     required
-                    value={form.values.city}
-                    onChange={(event) => form.setFieldValue('city', event.target.value)}
-                    style={styles.formInput}
+                    //name='city'
+                    value={city}
+                    onChangeText={setCity}
+                //onChange={handleInputChange}
+                // value={form.values.city}
+                // onChange={(event) => form.setFieldValue('city', event.target.value)}
+                // style={styles.formInput}
                 />
-                <ErrorMessage errorName={form.errors.city} errorId={"city-errorBox"} errorMessage={"invalid city name"} />
+
+                {/* <ErrorMessage errorName={form.errors.city} errorId={"city-errorBox"} errorMessage={"invalid city name"} /> */}
 
                 <Text style={styles.horizontal}>
                     <Text style={styles.label} htmlFor="state">State: </Text>
@@ -206,12 +258,17 @@ export default function CreateUser(props) {
                     required
                     maxLength="2"
                     autoCapitalize="characters" // why doesn't this work?
-                    //removed .toUpperCase() to fix error in value and onChange props
-                    value={(form.values.state)}
-                    onChange={(event) => form.setFieldValue('state', event.target.value)}
-                    style={styles.formInput}
+                    //name='state'
+                    value={state}
+                    onChangeText={setState}
+                // onChange={handleInputChange}
+                //removed .toUpperCase() to fix error in value and onChange props
+                //value={(form.values.state)}
+                // onChange={(event) => form.setFieldValue('state', event.target.value)}
+                // style={styles.formInput}
                 />
-                <ErrorMessage errorName={form.errors.state} errorId={"state-errorBox"} errorMessage={"Not a valid US state"} />
+
+                {/* <ErrorMessage errorName={form.errors.state} errorId={"state-errorBox"} errorMessage={"Not a valid US state"} /> */}
 
                 <Text style={styles.horizontal}>
                     <Text style={styles.label} htmlFor="zipCode">Zip Code: </Text>
@@ -222,13 +279,18 @@ export default function CreateUser(props) {
                     placeholder="Zip Code"
                     type="text"
                     required
-                    value={form.values.zipCode}
-                    onChange={(event) =>
-                        form.setFieldValue('zipCode', event.target.value)
-                    }
+                    //name='zipCode'
+                    value={zipCode}
+                    onChangeText={setZipcode}
+                    // onChange={handleInputChange}
+                    // value={form.values.zipCode}
+                    // onChange={(event) =>
+                    //     form.setFieldValue('zipCode', event.target.value)
+                    // }
                     style={styles.formInput}
                 />
-                <ErrorMessage errorName={form.errors.zipCode} errorId={"zipCode-errorBox"} errorMessage={"Must be a 5- or 9-digit number"} />
+
+                {/* <ErrorMessage errorName={form.errors.zipCode} errorId={"zipCode-errorBox"} errorMessage={"Must be a 5- or 9-digit number"} /> */}
 
                 <Text style={styles.horizontal}>
                     <Text style={styles.label} htmlFor="password">Password: </Text>
@@ -241,13 +303,16 @@ export default function CreateUser(props) {
                     required
                     autoComplete="new-password"
                     secureTextEntry="true"
-                    value={form.values.password}
-                    onChange={(event) =>
-                        form.setFieldValue('password', event.target.value)
-                    }
+                    // name='password'
+                    value={password}
+                    onChangeText={setPassword}
+                    //onChange={handleInputChange}
+                    // onChange={(event) =>
+                    //     form.setFieldValue('password', event.target.value)
+                    // }
                     style={styles.formInput}
                 />
-                <ErrorMessage errorName={form.errors.password} errorId={"password-errorBox"} errorMessage={"8-16 characters, Must contain a number, a letter and a special character"} />
+                {/* <ErrorMessage errorName={form.errors.password} errorId={"password-errorBox"} errorMessage={"8-16 characters, Must contain a number, a letter and a special character"} /> */}
 
                 <Text style={styles.horizontal}>
                     <Text style={styles.label} htmlFor="confirmPassword">Confirm Password: </Text>
@@ -260,15 +325,20 @@ export default function CreateUser(props) {
                     required
                     autoComplete="new-password"
                     secureTextEntry="true"
-                    value={form.values.confirmPassword}
-                    onChange={(event) =>
-                        form.setFieldValue('confirmPassword', event.target.value)
-                    }
+                    //name='confirmPassword'
+                    value={confirmPassword}
+                    onChangeText={setConformPassword}
+                    //onChange={handleInputChange}
+                    // onChange={(event) =>
+                    //     form.setFieldValue('confirmPassword', event.target.value)
+                    // }
                     style={styles.formInput}
                 />
-                <ErrorMessage errorName={form.errors.confirmPassword} errorId={"confirmPassword-errorBox"} errorMessage={"Passwords must match"} />
+                {/* <ErrorMessage errorName={form.errors.confirmPassword} errorId={"confirmPassword-errorBox"} errorMessage={"Passwords must match"} /> */}
 
-                <Pressable style={styles.button} title="Submit" onPress={form.onSubmit((values) => handleSubmit(values))}><Text>Submit</Text></Pressable>
+                <Pressable style={styles.button} title="Submit" onPress={handleSubmit}>
+                    {/* // {form.onSubmit((values) => handleSubmit(values))} */}
+                    <Text>Submit</Text></Pressable>
             </View>
         </View>
     );
