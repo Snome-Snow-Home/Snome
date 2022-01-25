26f6904fbb6dfd3bb104bb8011162fb9b84741be
Zach <zachary.bretz@gmail.com>
2022-01-25 10:13:28 -0700

    Replaced plaintext passwords with hashes and added object to map pass-hash to user

---
+++ backend/database/setup/test_data/passes_hashes.js
@@ -0,0 +1,51 @@
+let passes_hashes = [
+  {
+    username: "John Smith",
+    hash: "$2b$10$a5hvGfFJ.V9dz3KOnnbR7Ot908S3OPs0ZYHmGdx/yy5ZJVXcEz6.i",
+    password: "npK!3b",
+  },
+  {
+    username: "Jerry Seinfeld",
+    hash: "$2b$10$oPMUPrv6MMJ5yeaz45zvXOjyRw4txy7X05jEGari86lbpWz4NDXDm",
+    password: "tU$9K}",
+  },
+  {
+    username: "George Thomson",
+    hash: "$2b$10$Zkb9/FVNcGNN5Ni7hDZWqeMznO/DHuIoc2/DJ82h1XO64wR15qNiO",
+    password: "`3]qNt",
+  },
+  {
+    username: "Paula Gonzalez",
+    hash: "$2b$10$Ph9mIwHVWE123O/bBxqVgOM.rq6RIqDeJLINDz55Et9wrwF71X5Pi",
+    password: "MF7(m[",
+  },
+  {
+    username: "Charles Henry",
+    hash: "$2b$10$PbUmxFz21oj8szVfRAgq4eLBUdXK81rH3SKyiYvIz5xTcpdV1ptta",
+    password: "]mX4w?",
+  },
+  {
+    username: "Adam Stewart",
+    hash: "$2b$10$.CuA8zZ3rbohh3naM2ZXTu/tL3KOIif6EHqFIaJPSUnjqHetrkIIC",
+    password: ",2bwSX",
+  },
+  {
+    username: "David Henderson",
+    hash: "$2b$10$u0SrvxMAHl9nKIs.8rlwGut1Vqrr8NHToRh94WInFh/gaV5ylv6mC",
+    password: "'t2%F9",
+  },
+  {
+    username: "Madden Long",
+    hash: "$2b$10$3E4BQch.DZMnQQWwfaoni.2wQEvI6nCFfoc3YbZpaE26jllCRlzza",
+    password: "gK,4Jr",
+  },
+  {
+    username: "Shelby Harrington",
+    hash: "$2b$10$/syGEz4PbocHVsbOchylJuOOd1Efnx6Mp3.7Lic68pVhycPBbWPqC",
+    password: "R%t&6f",
+  },
+    username: "Cohen Riggs",
+    hash: "$2b$10$PQmJ8W5KTlAl9evr98a6ZeuO1pm1E7ofYQMPJ9pITMnyyxtuAh1lq",
+    password: "Bp2r$^"
+  }
+]
