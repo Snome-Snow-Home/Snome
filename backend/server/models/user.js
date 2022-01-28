c6f15c00e7f97e11ee4467fd35dfbc512c217ec7
mmetwalli96 <m.metwalli@outlook.com>
2022-01-28 11:49:58 -0500

    User Creation functionality is working

--- backend/server/models/user.js
+++ backend/server/models/user.js
@@ -16,7 +16,7 @@ module.exports = {
     }
   },
 
-  createUser: async ({nameText, email, street, city, state, zipCode, password}) => {
+  createUser: async ({nameText, username, email, street, city, state, zipCode, password}) => {
     try {
       const addressId = await db.one(`
         INSERT INTO address (
@@ -41,6 +41,7 @@ module.exports = {
       const userId = await db.one(`
         INSERT INTO snome_user (
           id,
+          user_name,
           name,
           about,
           email,
@@ -51,6 +52,7 @@ module.exports = {
         )
         VALUES (
           (SELECT MAX(id) FROM snome_user) +1,
+          '${username}',
           '${nameText}',
           'placeholder',
           '${email}',
