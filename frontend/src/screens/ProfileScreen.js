343f9d3feeb49bc82909358b2fbb37eb2c773c96
Amy Moseman <amoseman18@gmail.com>
2022-03-03 21:27:32 -0700

    styled profile page and added avatar option

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react'
-import { View, Text, Pressable, StyleSheet } from 'react-native'
+import { ScrollView, Text, Pressable, StyleSheet } from 'react-native'
 // import CreateUser from "../components/CreateUser";
 import UserContext from '../Context/UserContext'
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Avatar, Button, Title } from 'react-native-paper';
 
 
 const ProfileScreen = () => {
@@ -26,37 +27,49 @@ const ProfileScreen = () => {
 
 
     return (
-        <View>
-            <Text>Profile</Text>
-            <Text> Your username is: {context.user_data.username}</Text>
+        <ScrollView>
+            <Title style={styles.header}>Profile Settings</Title>
+            <Avatar.Image style={{ margin: 10 }} size={40} source={require('../pics/avatar.png')} />
+            <Text> {context.user_data.username}</Text>
             {/* // <Text>{userData}</Text> */}
             {/* {userData.length && userData.map(user => <div>{user.user_data}</div>)} */}
             {/* <CreateUser /> */}
-
-            <Pressable style={styles.button} title="logout" onPress={logout}>
+            {'\n'}
+            {'\n'}
+            {'\n'}
+            <Button mode="contained" style={styles.button} title="logout" onPress={logout}>
                 <Text>Logout</Text>
-            </Pressable>
-        </View>
+            </Button >
+        </ScrollView>
     )
 }
 
 
 
 const styles = StyleSheet.create({
-    button: {
+    // button: {
+    //     alignItems: 'center',
+    //     justifyContent: 'center',
+    //     paddingVertical: 12,
+    //     paddingHorizontal: 32,
+    //     borderRadius: 4,
+    //     elevation: 3,
+    //     backgroundColor: "#448EB1",
+    //     color: "white",
+    //     fontFamily: 'Arial',
+    //     width: "50%",
+    //     marginLeft: "25%",
+    //     marginRight: "25%",
+    //     marginTop: 20
+    // },
+    header: {
         alignItems: 'center',
         justifyContent: 'center',
-        paddingVertical: 12,
-        paddingHorizontal: 32,
-        borderRadius: 4,
-        elevation: 3,
-        backgroundColor: "#448EB1",
-        color: "white",
-        fontFamily: 'Arial',
-        width: "50%",
-        marginLeft: "25%",
-        marginRight: "25%",
-        marginTop: 20
+        fontSize: 50
+    },
+    button: {
+        width: '25%',
+        margin: 10
     }
 })
 
